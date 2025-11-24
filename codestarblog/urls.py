e5cf9d404d8fae934dc857acdbee2bad8758d81d
Brianon97 <brianon1997@gmail.com>
2025-11-24 09:23:08 +0000

    home page

--- codestarblog/urls.py
+++ codestarblog/urls.py
@@ -20,7 +20,8 @@ from blog import views as index_views
 from about import views as about_views
 
 urlpatterns = [
-    path('hello/', index_views.index, name='index'),
+    path('blog/', index_views.index, name='index'),
     path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
